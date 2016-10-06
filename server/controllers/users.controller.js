4e10d9b79a15e57fecdb7098057afc7477f15775
Felipe Nava <felipe.nava.co@gmail.com>
2016-10-06 01:01:08 -0500

    Errors helpers and logic is in users model now

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -5,7 +5,9 @@
 const UserModel = require('../../src/models/users.model.js'),
 	  mongoose = require('mongoose'),
       Validators = require('../../src/helpers/validators.helper.js'),
+      Errors = require('../../src/helpers/errors.helper.js'),
 	  express = require('express'),
+      errors = new Errors(),
       validators = new Validators();
 
 let usersRouter = express.Router();
@@ -13,73 +15,22 @@ let usersRouter = express.Router();
 usersRouter.route('/')
 	.post((req, res) => {
 
-	    UserModel.validateNewUser(req.body, (err, user) => {
+	    UserModel.create(req.body, (err, user) => {
 
 	    	if (err) {
 	    		res.status(400).json(err);
 	    	} else {
-
-				let user = new UserModel({
-			    	'name': req.body.name,
-			    	'username': req.body.username,
-			    	'email': req.body.email,
-			    	'password': req.body.password,
-			    	'plan': 0,
-			    	'created_date': new Date()
-			    });
-
-				user.add( (err, user, affected) => {
-					if (err) {
-						res.status(400).send(err);
-				    } else {
-				    	res.status(200).json({ idUser: user._id, message: 'User created' });	
-			    	}
-				});
+                res.status(200).json(user);
             }
 	    });
 		
 	}).get((req, res) => {
-        let query = req.get('query'),
-            dateStart,
-            dateEnd;
 
-        if (req.get('dateStart') && req.get('dateStart')) {
-            if (Date.Parse(query.dateStart)) {
-                dateStart = new Date(query.dateStart);
-            } else   {
-                res.status(400).json({ message: 'The start date is not a correct date', errorCode: 40010});
-            }
-
-            if (Date.Parse(query.dateEnd)) {
-                dateEnd = new Date(query.dateEnd);
-            } else {
-                res.status(400).json({ message: 'The end date is not a correct date', errorCode: 40011});
-            }
+        UserModel.get( req, (err, users) => {
             
-            if (typeof query === 'undefined') {
-                query = { dateStart : dateStart, dateEnd : dateEnd};
-            } else if (validators.validateJSON(query)) {
-                query = JSON.parse(query);
-                query.dateStart = dateStart;
-                query.dateEnd = dateEnd;
-            } else{
-                res.status(400).json({ message: 'Query is not setted correctly', errorCode: 4008});
-            }
-        }
-
-        if (typeof query === 'undefined') {
-            res.status(400).json({ message: 'Query is empty', errorCode: 4009 });
-        }
-
-        if (validators.validateJSON(query)) {
-                query = JSON.parse(query);
-            } else{
-                res.status(400).json({ message: 'Query is not setted correctly', errorCode: 4008});
-            }
-
-        UserModel.get( query, (err, users) => {
             if (err) {
-                res.status(400).send(err);
+                res.status(400).json(err);  
+
             } else {
                 res.status(200).json(users);
             }
@@ -88,79 +39,39 @@ usersRouter.route('/')
 
 usersRouter.route('/:user_id')
 	.get(function(req, res) {
+        
+        UserModel.getById(req.params.user_id, (err, user) => {
+            if (err) res.status(400).json(err);
 
-        if (!validators.validateId(req.params.user_id)) {
-            res.status(400).json({ message: 'ID value is not correct', errorCode: 40015});
-        } else {
-            UserModel.findById(req.params.user_id, '_id name username email plan created_date', function(err, user) {
-            if (err)
-                res.send(err);
-            
-            if (user && user.toObject()) {
+            if (user) {
                 res.status(200).json(user);
-            } else {
-                res.status(400).json({ message: 'User not found.', errorCode: 40012 });    
             }
-            
-            });
-        }
+        });
         
     }).put(function(req, res) {
 
-        if (!validators.validateId(req.params.user_id)) {
-            res.status(400).json({ message: 'ID value is not correct', errorCode: 40015});
-        } else {
             UserModel.update(req.params.user_id, req.body, (err, user, affected) => {
                 if (err) {
-                    if (err.message === 'Error: 4002') {
-                        return res.status(400).json({ message: 'Username is duplicated', errorCode: 4002});   
-                    }
-
-                    if (err.message === 'Error: 4005') {
-                        return res.status(400).json({ message: 'Email is duplicated', errorCode: 4005});   
-                    }
-
-                    if (err.message === 'Error: 40012') {
-                        return res.status(400).json({ message: 'User not found', errorCode: 40012});   
-                    }
-
-                    if (err.message === 'Error: 4004') {
-                        return res.status(400).json({ message: 'Email is not correct', errorCode: 4004});   
-                    }
-
-                    return res.status(400).json({ message: 'Something went wrong D:', errorCode: 401 });   
-                }
-
-                if (affected > 0) {
-                    res.status(200).json({ message: 'User is updated'});
+                    return res.status(400).json(err);   
                 } else {
-                    res.status(400).json({ message: 'User was not updated', errorCode: 40017});
+                    if (affected > 0) {
+                        res.status(200).json({ message: 'User updated'});
+                    } else {
+                        res.status(400).json({ message: errors.getMessage(40017), errorCode: 40017});
+                    }
                 }
             });
-        }
 
     }).delete(function(req, res) {
-        
-        if (validators.validateId(req.params.user_id)) {
-            UserModel.findById(req.params.user_id, (err, user) => {
 
-                if (user){
-                    user.remove(function(err, removed) {
-                        if (err)
-                            res.send(err);
+        UserModel.delete(req.params.user_id, (err, removed) => {
+            if (err) {
+                res.status(400).json(err); 
+            } else {
+                res.status(200).json(removed);
+            }
 
-                        console.log('Removed:', removed);
-                        res.status(200).json({ message: 'Successfully deleted' });
-                    });    
-                } else {
-                    res.status(200).json({ message: 'User not found', errorCode:  40012});
-                }
-
-            });
-            
-        } else {
-            res.status(400).json({ message: 'ID value is not correct', errorCode: 40015});
-        }
+        });
         
     });
 
--- src/models/users.model.js
+++ src/models/users.model.js
@@ -5,6 +5,7 @@
 const mongoose = require('mongoose'),
 	  Schema = mongoose.Schema,
 	  Validators = require('../helpers/validators.helper.js'),
+	  Errors = require('../helpers/errors.helper.js'),
 	  userSchema = new Schema({
 				name: {
 					type: String,
@@ -28,6 +29,8 @@ const mongoose = require('mongoose'),
 				created_date: Date
 			}),
 	  UserModel = mongoose.model('User', userSchema),
+	  env = process.env.NODE_ENV || 'dev',
+	  errors = new Errors(env),
 	  validators = new Validators();
 
 class Users extends UserModel{
@@ -42,37 +45,55 @@ class Users extends UserModel{
 
 }
 
-Users.validateNewUser = (data, callback) => {
+Users.create = (data, callback) => {
 	if (!data.name) {
-		return callback({ message: 'Name is empty', errorCode: 4006},'');
+		return callback({ message: errors.getMessage(4006), errorCode: 4006},'');
 	}
 
 	if (!data.username) {
-		return callback({ message: 'Username is empty', errorCode: 4001},'');
+		return callback({ message: errors.getMessage(4001), errorCode: 4001},'');
 	}
 
 	if (!data.email) {
-		return callback({ message: 'Email is empty', errorCode: 4003 }, '');
+		return callback({ message: errors.getMessage(4003), errorCode: 4003 }, '');
 	} else if (!validators.validateEmail(data.email)) {
-		return callback({ message: 'Email is not correct', errorCode: 4004 }, '');
+		return callback({ message: errors.getMessage(4004), errorCode: 4004 }, '');
 	}
 
 	if (!data.password) {
-		return callback({ message: 'Password is empty', errorCode: 4007},'');
+		return callback({ message: errors.getMessage(4007), errorCode: 4007},'');
 	}
 
 	let query = { username: data.username};
 
 	UserModel.find(query, (err, user) => {
 		if (user.length > 0){
-			return callback({ message: 'Username is duplicated', errorCode: 4002 }, '');
+			return callback({ message: errors.getMessage(4002), errorCode: 4002 }, '');
 		} else {
+			
 			let query = { email: data.email };
+
 			UserModel.find(query, (err, user) => {
 				if (user.length > 0){
-					return callback({ message: 'Email is duplicated', errorCode: 4005 }, '');
+					return callback({ message: errors.getMessage(4005), errorCode: 4005 }, '');
 				} else {
-					return callback('','');
+					
+					let user = new UserModel({
+				    	'name': data.name,
+				    	'username': data.username,
+				    	'email': data.email,
+				    	'password': data.password,
+				    	'plan': 0,
+				    	'created_date': new Date()
+				    });
+
+					user.add( (err, user, affected) => {
+						if (err) {
+							return callback(err, '');
+					    } else {
+					    	return callback('', { idUser: user._id, message: 'User created' });
+				    	}
+					});
 				}
 			});				
 		}
@@ -80,7 +101,50 @@ Users.validateNewUser = (data, callback) => {
 };
 
 
-Users.get = (query, callback) => {
+Users.get = (req, callback) => {
+	let query = req.get('query'),
+		dateStart,
+		dateEnd;
+
+	if (req.get('dateStart') && req.get('dateStart')) {
+
+            if (Date.Parse(query.dateStart)) {
+                dateStart = new Date(query.dateStart);
+            } else   {
+                return callback({ message: errors.getMessage(40010), errorCode: 40010}, '');
+            }
+
+            if (Date.Parse(query.dateEnd)) {
+                dateEnd = new Date(query.dateEnd);
+            } else {
+                return callback({ message: errors.getMessage(40011), errorCode: 40011}, '');
+            }
+            
+            if (typeof query === 'undefined') {
+                query = { dateStart : dateStart, dateEnd : dateEnd};
+            } else {
+            	try {
+				    query = JSON.parse(query);
+                	query.dateStart = dateStart;
+                	query.dateEnd = dateEnd;
+				}
+				catch(e) {
+					return callback({ message: errors.getMessage(4008), errorCode: 4008}, '');   
+				}
+        	}
+        }
+
+        if (typeof query === 'undefined') {
+            return callback({ message: errors.getMessage(4009), errorCode: 4009 }, '');
+        }
+
+		try {
+		   query = JSON.parse(query);
+		}
+		catch(e) {
+			return callback({ message: errors.getMessage(4008), errorCode: 4008}, '');   
+		}
+
 	UserModel.find(query, '_id name username email plan created_date', (err, users) => {
 		return callback('',users);
 	});
@@ -88,95 +152,155 @@ Users.get = (query, callback) => {
 
 Users.update = (id, data, callback) => {
 	if (!id) {
-		return callback({ message: 'ID is empty', errorCode : 40013}, '', 0);
+		return callback({ message: errors.getMessage(40013), errorCode : 40013}, '', 0);
 	}
 
-	if (!data || validators.isEmptyObject(data)) {
-		return callback({ message: 'Data is empty', errorCode: 40014}, '', 0);
-	}
-	mongoose.Promise = global.Promise;
-	
-	let checkCommonData = (user) => {
-		if (user) {
 
-			if (data.name && data.name !== '')
-        		user.name = data.name; 
+    if (!validators.validateId(id)) {
+        return callback({ message: errors.getMessage(40015), errorCode: 40015}, '');
+    } else {
 
-    		if (data.plan && data.plan !== '')
-        		user.plan = data.plan;
-
-		} else {
-			throw new Error(40012);
+		if (!data || validators.isEmptyObject(data)) {
+			return callback({ message: errors.getMessage(40014), errorCode: 40014}, '', 0);
 		}
-
-		return user;
-	};
-
-	let checkUsername = (user) => {
-		if (data.username && user.username !== data.username) {
-			let query = { username:  data.username };
-
-			return UserModel.find(query).exec()
-			.then((userFound) => {
-				if (userFound.length > 0) {
-					user = null;
-					throw new Error(4002);
-				} else {
-					user.username = data.username;
-					return user;
-				}
-
-			})
-			.catch((err) => {throw new Error(err); });
-		}
-
-		return user;
-	};
-
-	let checkEmail = (user) => {
-		if (data.email && user.email !== data.email) {
-
-			if (!validators.validateEmail(data.email)) {
-				throw new Error(4004);
-			}
-			let query = { email:  data.email };
-
-			return UserModel.find(query).exec()
-			.then((userFound) => { 
-				if (userFound.length > 0) {
-					user = null;
-					throw new Error(4005);
-				} else {
-					user.email = data.email;
-					return user;
-				}
-			})
-			.catch((err) => { throw new Error(err); });
-		}
-
-		return user;
-	};
-
-	let errorFound = (err) => {
-		return callback(err);
-	};
-
-	let saveChanges = (user) => {
-		return user.save(function(err, user, affected) {
-            if (err) {
-            	throw new Error(err);
-            }
-            return callback('', user, affected);
-        });
-	};
-
-	let findById = UserModel.findById(id).exec();
+		mongoose.Promise = global.Promise;
 		
-		findById.then(checkCommonData)
-		.then(checkUsername)
-		.then(checkEmail)
-		.then(saveChanges)
-		.catch(errorFound);
+		let checkCommonData = (user) => {
+			if (user) {
+
+				if (data.name && data.name !== '')
+	        		user.name = data.name; 
+
+	    		if (data.plan && data.plan !== '')
+	        		user.plan = data.plan;
+
+			} else {
+				throw new Error(40012);
+			}
+
+			return user;
+		};
+
+		let checkUsername = (user) => {
+			if (data.username && user.username !== data.username) {
+				let query = { username:  data.username };
+
+				return UserModel.find(query).exec()
+				.then((userFound) => {
+					if (userFound.length > 0) {
+						user = null;
+						throw new Error(4002);
+					} else {
+						user.username = data.username;
+						return user;
+					}
+
+				})
+				.catch((err) => {throw new Error(err); });
+			}
+
+			return user;
+		};
+
+		let checkEmail = (user) => {
+			if (data.email && user.email !== data.email) {
+
+				if (!validators.validateEmail(data.email)) {
+					throw new Error(4004);
+				}
+				let query = { email:  data.email };
+
+				return UserModel.find(query).exec()
+				.then((userFound) => { 
+					if (userFound.length > 0) {
+						user = null;
+						throw new Error(4005);
+					} else {
+						user.email = data.email;
+						return user;
+					}
+				})
+				.catch((err) => { throw new Error(err); });
+			}
+
+			return user;
+		};
+
+		let errorFound = (err) => {
+			if (err.message === 'Error: 4002') {
+                return callback({ message: errors.getMessage(4002), errorCode: 4002}, '', '');   
+            }
+
+            if (err.message === 'Error: 4005') {
+                return callback({ message: errors.getMessage(4005), errorCode: 4005}, '', '');   
+            }
+
+            if (err.message === 'Error: 40012') {
+                return callback({ message: errors.getMessage(40012), errorCode: 40012}, '', '');   
+            }
+
+            if (err.message === 'Error: 4004') {
+                return callback({ message: errors.getMessage(4004), errorCode: 4004}, '', '');   
+            }
+
+            return callback({ message: errors.getMessage(401), errorCode: 401 }, '', '');   
+		};
+
+		let saveChanges = (user) => {
+			return user.save(function(err, user, affected) {
+	            if (err) {
+	            	throw new Error(err);
+	            }
+	            return callback('', user, affected);
+	        });
+		};
+
+		let findById = UserModel.findById(id).exec();
+			
+			findById.then(checkCommonData)
+			.then(checkUsername)
+			.then(checkEmail)
+			.then(saveChanges)
+			.catch(errorFound);
+	}
+};
+
+Users.getById = (id, callback) => {
+
+	if (!validators.validateId(id)) {
+            return callback({ message: errors.getMessage(40015), errorCode: 40015}, '');
+        } else {
+            UserModel.findById(id, '_id name username email plan created_date', function(err, user) {
+	            if (err)
+	                return callback(err, '');
+	            
+	            if (user && user.toObject()) {
+	                return callback('', user);
+	            } else {
+	            	return callback({ message: errors.getMessage(40012), errorCode: 40012 }, '');
+	            }
+            });
+        }
+};
+
+Users.delete = (id, callback) => {
+	if (validators.validateId(id)) {
+            UserModel.findById(id, (err, user) => {
+                if (user){
+                    user.remove(function(err, removed) {
+                        if (err)
+                            return callback(err, '');
+
+                        return callback('', { message: 'Successfully deleted' });
+                    });    
+                } else {
+                    return callback({ message: errors.getMessage(40012), errorCode:  40012}, '');
+                }
+
+            });
+        } else {
+            return callback({ message: errors.getMessage(40015), errorCode: 40015}, '');
+        }
 };
 
 module.exports = Users;
